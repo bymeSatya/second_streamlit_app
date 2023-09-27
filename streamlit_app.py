79a58e480322659bde7b3332ae1207205331bfe2
Satya Chaitanya <66238780+bymeSatya@users.noreply.github.com>
2023-09-27 11:25:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import pandas
 
 streamlit.title('😎This is My streamlit Application')
 streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
