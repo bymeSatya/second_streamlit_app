bc8441aee10804d8d6e4131ff03dc2645fb0f1fa
Satya Chaitanya <66238780+bymeSatya@users.noreply.github.com>
2023-09-27 11:21:44 +0530

    Update streamlit_app.py
    Added Line and Title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
+import streamlit
+import pandas
+import requests
+# import setuptools
+import snowflake.connector
+from urllib.error import URLError
 
+streamlit.title('😎This is My streamlit Application')
+streamlit.header('Breakfast Menu')
